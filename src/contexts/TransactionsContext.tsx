54f928c2b48739446425c76f7f45f05538b2c608
atilio ferreira <atilio.silvaferreira@gmail.com>
2023-07-27 20:44:54 -0300

    adicionando lint e axios, e integrando com get e post

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,5 @@
 import { ReactNode, createContext, useEffect, useState } from "react";
+import { api } from "../lib/axios";
 
 interface Transaction {
   id: number;
@@ -10,11 +11,20 @@ interface Transaction {
 }
 
 interface TransctionContextType {
-  transactions: Transaction[]
+  transactions: Transaction[];
+  fetchTransactions: (query?: string) => Promise<void>
+  createTransaction: (data: CreateTransactionInput) => Promise<void>
 }
 
 interface TransactionsProviderProps {
   children: ReactNode;
+}
+
+interface CreateTransactionInput {
+  description: string;
+  price: number;
+  category: string;
+  type: 'income' | 'outcome';
 }
 
 export const TransactionsContext = createContext({} as TransctionContextType);
@@ -22,19 +32,38 @@ export const TransactionsContext = createContext({} as TransctionContextType);
 export function TransactionsProvider({children}: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([])
       
-  async function loadTransactions() {
-    const response = await fetch('http://localhost:3333/transactions');
-    const data = await response.json();
+  async function fetchTransactions(query?: string) {
+    const response = await api.get('/transactions', {
+      params: {
+        _sort: 'createdAt',
+        _order: 'desc',
+        q: query
+      }
+    })
 
-    setTransactions(data);
+    setTransactions(response.data);
+  }
+
+  async function createTransaction(data: CreateTransactionInput) {
+    const { description, price, category, type} = data;
+
+    const response = await api.post('transactions', {
+      description, 
+      price, 
+      category, 
+      type,
+      createdAt: new Date()
+    });
+
+    setTransactions(state => [response.data, ...state]);
   }
 
   useEffect(() => {
-    loadTransactions();
+    fetchTransactions();
   }, []);
   
   return (
-    <TransactionsContext.Provider value={{ transactions }}>
+    <TransactionsContext.Provider value={{ transactions, fetchTransactions, createTransaction }}>
       {children}
     </TransactionsContext.Provider>
   )
